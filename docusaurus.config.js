6f1762e22db71bcbea4969221c4d4791f5766ae6
onecheck <1548767361@qq.com>
2025-05-08 01:54:20 +0800

    feat: update homepage

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,123 +4,137 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (module.exports = {
-  title: 'Dayu',
-  // disableHeaderTitle: true,
-  // tagline: 'Dayu is all you need',
-  tagline: 'Provide infrastructure for cloud-edge collaborative stream data analysis.',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  favicon: 'img/dayu-logo.png',
+    title: 'Dayu',
+    // disableHeaderTitle: true,
+    // tagline: 'Dayu is all you need',
+    tagline: 'Provide infrastructure for cloud-edge collaborative stream data analysis.',
+    url: 'https://your-docusaurus-test-site.com',
+    baseUrl: '/',
+    favicon: 'img/dayu-logo.png',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'dayu-autostreamer', // Usually your GitHub org/user name.
-  projectName: 'dayu', // Usually your repo name.
-  trailingSlash: false,
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'dayu-autostreamer', // Usually your GitHub org/user name.
+    projectName: 'dayu', // Usually your repo name.
+    trailingSlash: false,
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  // staticDirectories: ['public', 'static'],
+    onBrokenLinks: 'warn',
+    onBrokenMarkdownLinks: 'warn',
+    // staticDirectories: ['public', 'static'],
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'zh'],
-    localeConfigs: {
-      en: {
-        label: 'English',
-      },
-      zh: {
-        label: '简体中文',
-      },
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en', 'zh'],
+        localeConfigs: {
+            en: {
+                label: 'English',
+            },
+            zh: {
+                label: '简体中文',
+            },
+        },
     },
-  },
 
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/dayu-autostreamer/dayu-autostreamer.github.io/tree/main',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/dayu-autostreamer/dayu-autostreamer.github.io/tree/main',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    editUrl: 'https://github.com/dayu-autostreamer/dayu-autostreamer.github.io/tree/main',
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    editUrl:
+                        'https://github.com/dayu-autostreamer/dayu-autostreamer.github.io/tree/main',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        // title: 'Dayu',
-        logo: {
-          alt: 'Dayu Site Logo',
-          src: 'img/dayu.png',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'introduction/why-dayu',
-            position: 'left',
-            label: 'Documentation',
-          },
-          { to: '/blog', label: 'Blog', position: 'left' },
-          {
-            href: 'https://github.com/dayu-autostreamer/dayu',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/introduction/why-dayu',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Github Issue',
-                href: 'https://github.com/dayu-autostreamer/dayu/issues',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/dayu-autostreamer/dayu',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} The Dayu Authors. All rights reserved.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+        ({
+            navbar: {
+                // title: 'Dayu',
+                logo: {
+                    alt: 'Dayu Site Logo',
+                    src: 'img/dayu.png',
+                },
+                items: [
+                    {
+                        type: 'doc',
+                        docId: 'introduction/why-dayu',
+                        position: 'left',
+                        label: 'Documentation',
+                    },
+                    {to: '/blog', label: 'Blog', position: 'left'},
+                    {
+                        href: 'https://github.com/dayu-autostreamer/dayu',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Docs',
+                        items: [
+                            {
+                                label: 'Introduction',
+                                to: '/docs/introduction/why-dayu',
+                            },
+                            {
+                                label: 'Tutorial',
+                                to: '/docs/getting-start/quick-start',
+                            },
+                            {
+                                label: 'Development',
+                                to: '/docs/developer-guide/how-to-develop',
+                            },
+
+                        ],
+                    },
+                    {
+                        title: 'Community',
+                        items: [
+                            {
+                                label: 'Github Issue',
+                                href: 'https://github.com/dayu-autostreamer/dayu/issues',
+                            },
+                            {
+                                label: 'Contact Us',
+                                to: '/docs/community/contact-us',
+                            },
+
+                        ],
+                    },
+                    {
+                        title: 'More',
+                        items: [
+                            {
+                                label: 'GitHub',
+                                href: 'https://github.com/dayu-autostreamer/dayu',
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} Dayu Project Authors. All rights reserved.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
 });
--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -3,62 +3,67 @@ import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
-  {
-    title: 'Negative Feedback',
-    Svg: require('../../static/img/dayu-feedback.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on Stream Task',
-    Svg: require('../../static/img/dayu-stream.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Self-Adapt',
-    Svg: require('../../static/img/dayu-adapt.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+    {
+        title: 'Stream Data Analysis',
+        Svg: require('../../static/img/dayu-stream.svg').default,
+        description: (
+            <>
+                Dayu focuses on ai-driven stream data analysis,
+                natively supporting parallel multi-stream processing/monitoring/scheduling with heterogeneous user-defined applications.
+            </>
+        ),
+    },
+
+    {
+        title: 'Distributed Cloud-Edge Collaboration',
+        Svg: require('../../static/img/dayu-cloud-edge.svg').default,
+        description: (
+            <>
+                Based on a container orchestration framework KubeEdge,
+                dayu is applicable to distributed cloud-edge collaborative scenarios and can be scale to heterogeneous devices.
+
+
+            </>
+        ),
+    },
+
+    {
+        title: 'Adaptive System Scheduling',
+        Svg: require('../../static/img/dayu-schedule.svg').default,
+        description: (
+            <>
+                Dayu can be extended to scheduling topics of different granularity with flexible structure.
+                Adaptive scheduling algorithms thereby can be implemented on dayu for system support.
+
+            </>
+        ),
+    },
 ];
 
-function Feature({ Svg, title, description }) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({Svg, title, description}) {
+    return (
+        <div className={clsx('col col--4')}>
+            <div className="text--center">
+                <Svg className={styles.featureSvg} alt={title}/>
+            </div>
+            <div className="text--center padding-horiz--md">
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
